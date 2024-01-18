266dce454946f9799e6894521072765bb2e86a44
AFSAL333 <afsalafs8008@gmail.com>
2024-01-18 21:43:50 +0530

    promise

--- Asynchronous/promise.js
+++ Asynchronous/promise.js
@@ -33,3 +33,42 @@ fetchData()
   .catch((error) => {
     console.error("Error:", error);
   });
+
+//? Promise methods:
+
+// then(): This method is used to handle the fulfilled state of a promise.
+
+// catch(): This method is used to handle the rejected state of a promise.
+
+// finally(): This method is used to specify a function that should be executed when
+// the promise is either fulfilled or rejected.
+
+// Promise.resolve(): This method returns a resolved promise with a given value.
+
+// Promise.reject(): This method returns a rejected promise with a given reason.
+
+// Promise.all(): This method takes an array of promises and returns a single promise
+// that resolves when all the promises in the array have resolved.
+{
+  const promises = [promise1, promise2, promise3];
+  Promise.all(promises)
+    .then((results) => {
+      // Handle the results when all promises are fulfilled
+    })
+    .catch((error) => {
+      // Handle an error if any of the promises is rejected
+    });
+}
+
+// Promise.race(): This method takes an array of promises and returns a single promise
+// that resolves or rejects as soon as one of the promises in the array resolves or rejects.
+{
+  const promises = [promise1, promise2, promise3];
+  Promise.race(promises)
+    .then((firstResult) => {
+      // Handle the result of the first promise that resolves
+    })
+    .catch((firstError) => {
+      // Handle the error of the first promise that rejects
+    });
+}
