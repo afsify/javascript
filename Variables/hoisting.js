10b38b94e0bdbec8c5e67a81f6a7e453858a48c0
AFSAL333 <afsalafs8008@gmail.com>
2024-01-18 20:25:26 +0530

    shadowing

---
+++ Variables/hoisting.js
@@ -0,0 +1,20 @@
+//! ======================================== Hoisting ========================================
+
+// Hoisting in JavaScript is a behavior where variable and function declarations are moved to the top
+// of their respective scopes during the compilation phase, allowing them to be used before they are
+// formally declared in the code.
+
+// When variables are declared using the var keyword, they are hoisted to the top of their scope,
+// but only the declarations are hoisted, not the initializations.
+
+console.log(x); // undefined
+var x = 5;
+console.log(x); // 5
+
+// Function declarations are also hoisted to the top of their scope.
+
+sayHello(); // "Hello, world!"
+
+function sayHello() {
+  console.log("Hello, world!");
+}
---
+++ Variables/shadowing.js
@@ -0,0 +1,16 @@
+//! ======================================== Shadowing ========================================
+
+// When a variable declared within a certain scope has the same name as a variable in an outer scope.
+// The inner variable "shadows" or hides the outer variable of the same name, making the outer variable
+// temporarily inaccessible within the inner scope.
+
+let x = 10;
+
+function example() {
+  let x = 5; // This 'x' shadows the outer 'x' within this function scope
+  console.log(x); // Outputs 5, referring to the inner 'x'
+}
+
+example();
+
+console.log(x); // Outputs 10, referring to the outer 'x'
