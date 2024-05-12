3e8153da2a54547bb236edb38b72d50ff47a6b42
AFSAL333 <afsalafs8008@gmail.com>
2024-05-12 20:53:15 +0530

    added for of

--- Loop/for.of.js
+++ Loop/for.of.js
@@ -1,1 +1,11 @@
 //! ======================================== For Of Loop ========================================
+
+// The JavaScript for of statement loops through the values of an iterable object.
+// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
+
+const cars = ["BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let x of cars) {
+  text += x;
+}
