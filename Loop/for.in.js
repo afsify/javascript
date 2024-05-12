671347c9b14187d3d86db527ff3c543b7e6003ce
AFSAL333 <afsalafs8008@gmail.com>
2024-05-12 20:50:27 +0530

    added for in

--- Loop/for.in.js
+++ Loop/for.in.js
@@ -2,7 +2,7 @@
 
 // for in statement loops through the properties of an Object
 
-const person = {fname:"John", lname:"Doe", age:25};
+const person = { fname: "John", lname: "Doe", age: 25 };
 
 let text = "";
 for (let x in person) {
