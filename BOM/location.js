de39ab34a2480d6c11b943bf37cc0575b8a7cd01
AFSAL333 <afsalafs8008@gmail.com>
2024-05-15 20:31:47 +0530

    added definition

--- BOM/location.js
+++ BOM/location.js
@@ -1,1 +1,3 @@
 //! ======================================== Location ========================================
+
+// The window.location object can be used to get the current page address (URL) and to redirect the browser to a new page.
--- BOM/screen.js
+++ BOM/screen.js
@@ -1,1 +1,16 @@
 //! ======================================== Screen ========================================
+
+// The window.screen object can be written without the window prefix.
+
+screen.width
+screen.height
+screen.availWidth
+screen.availHeight
+screen.colorDepth
+screen.pixelDepth
+
+// The screen.width property returns the width of the visitor's screen in pixels.
+
+document.getElementById("demo").innerHTML =
+"Screen Width: " + screen.width;
+
