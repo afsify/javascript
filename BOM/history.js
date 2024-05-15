5ddbaca4836bfa006c2fc587be355651c315e5a4
AFSAL333 <afsalafs8008@gmail.com>
2024-05-15 20:38:25 +0530

    added definition for history

--- BOM/history.js
+++ BOM/history.js
@@ -1,1 +1,25 @@
 //! ======================================== History ========================================
+
+// The window.history object contains the browsers history.
+
+history.back()
+// - same as clicking back in the browser
+history.forward()
+// - same as clicking forward in the browser
+
+// The history.back() method loads the previous URL in the history list.
+
+{/* <html>
+<head>
+<script> */}
+function goBack() {
+  window.history.back()
+}
+{/* </script>
+</head>
+<body>
+
+<input type="button" value="Back" onclick="goBack()">
+
+</body>
+</html> */}
--- BOM/location.js
+++ BOM/location.js
@@ -2,3 +2,17 @@
 
 // The window.location object can be used to get the current page address (URL) and to
 // redirect the browser to a new page.
+
+window.location.href
+// returns the href (URL) of the current page
+window.location.hostname
+// returns the domain name of the web host
+window.location.pathname
+// returns the path and filename of the current page
+window.location.protocol
+// returns the web protocol used (http: or https:)
+window.location.assign()
+// loads a new document
+
+document.getElementById("demo").innerHTML =
+"Page location is " + window.location.href;
