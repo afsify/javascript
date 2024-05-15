2ac6d444fd11a4355fb12eb2503e9b77778214e9
AFSAL333 <afsalafs8008@gmail.com>
2024-05-15 20:42:37 +0530

    added definition for cookie

--- BOM/cookie.js
+++ BOM/cookie.js
@@ -1,1 +1,13 @@
 //! ======================================== Cookie ========================================
+
+// Cookies are data, stored in small text files, on your computer.
+// When a web server has sent a web page to a browser, the connection is shut down, and
+// the server forgets everything about the user.
+
+// Cookies were invented to solve the problem "how to remember information about the user":
+
+// When a user visits a web page, his/her name can be stored in a cookie.
+// Next time the user visits the page, the cookie "remembers" his/her name.
+// Cookies are saved in name-value pairs like:
+
+document.cookie = "username=John Doe";
--- BOM/navigator.js
+++ BOM/navigator.js
@@ -1,1 +1,15 @@
 //! ======================================== Navigator ========================================
+
+// The window.navigator object contains information about the visitor's browser.
+
+// navigator.cookieEnabled
+// navigator.appCodeName
+// navigator.platform
+
+// The cookieEnabled property returns true if cookies are enabled, otherwise false:
+
+{/* <p id="demo"></p>
+<script> */}
+document.getElementById("demo").innerHTML =
+"cookiesEnabled is " + navigator.cookieEnabled;
+{/* </script> */}
