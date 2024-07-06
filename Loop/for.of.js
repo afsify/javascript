fe807f2643edbf6eee0c8d5f9d3e7cb48487ae44
AFSAL333 <afsalafs8008@gmail.com>
2024-07-06 16:14:48 +0530

    for of added

--- Loop/for.of.js
+++ Loop/for.of.js
@@ -9,3 +9,23 @@ let text = "";
 for (let x of cars) {
   text += x;
 }
+
+// for...in can iterate over inherited properties as well,
+// while for...of only iterates over the values of the specific iterable.
+// for...of works with any object that has an iterable protocol implemented
+// (an object with a Symbol.iterator method).
+
+const array = ['a', 'b', 'c'];
+
+// for...in
+for (const index in array) {
+  console.log(index); // '0', '1', '2' (indices as strings)
+  console.log(array[index]); // 'a', 'b', 'c'
+}
+
+// for...of
+for (const value of array) {
+  console.log(value); // 'a', 'b', 'c' (values directly)
+}
+
+// use for...in for object properties and for...of for iterable values.
